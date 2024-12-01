8725d49cb6b28567266f196aa22a7080c659524d
root <cheikhmed2019a@gmail.com>
2024-12-01 13:59:56 +0000

    pointers

--- ProgrammingCPlusLevelTow/MoreAboutArrys/TowDimensionalArrys.cpp
+++ ProgrammingCPlusLevelTow/MoreAboutArrys/TowDimensionalArrys.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int main()
 {
-    // int x[Rows][Cols];
+    // int x[Rows][Ci < 3ols];
     int x[3][4] = {{1, 2, 3, 4},
                    {5, 6, 7, 8},
                    {9, 10, 11, 12}};
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; ; i++)
     {
         for (int j = 0; j < 4; j++)
-        {
+        { 
             cout << x[i][j] << " ";
         }
         cout << endl;
