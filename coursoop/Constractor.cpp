19446e49a6214d5220c70229151c4afe5a2a548d
cheikh-sidi-taher <cheikhmed2019a@gmail.com>
2025-08-03 22:46:49 +0000

    ton message

--- coursoop/Constractor.cpp
+++ coursoop/Constractor.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
+class clsPerson
+{
+public:
+    string FirstName;
+    string LastName;
+
+    string getFullName()
+    {
+        return FirstName + " " + LastName;
+    }
+};
+
+int main()
+{
+
+    clsPerson Person1, Person2;
     
+
+    Person1.FirstName = "cheikh";
+    Person1.LastName = "taher";
+
+
+
+    cout << Person1.getFullName() << endl;
+     cout << Person2.getFullName() << endl;
 }
